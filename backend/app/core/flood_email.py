323f20a1e30ec3f5510f25e94b2a239ddc6bd7ac
aaron <changxing.wu@gmx.com>
2023-12-09 21:29:42 +1100

    update the env for gmail sending message

--- backend/app/core/flood_email.py
+++ backend/app/core/flood_email.py
@@ -1,22 +1,37 @@
 import os
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 import smtplib
 import ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-password = os.environ.get("EMAIL_PASSWORD")
+email_address = os.environ.get("EMAIL_HOST_USER")
+email_password = os.environ.get("EMAIL_HOST_PASSWORD")
 
-def send_email(sender, receiver, subject, message, attachments=None):
+
+if not email_address or not email_password:
+    raise ValueError("EMAIL_ADDRESS and EMAIL_PASSWORD environment variables must be set")
+
+def send_email(receiver, subject, message, attachments=None):
     email = MIMEMultipart()
-    email["From"] = sender
+    email["From"] = "me@aaron404.com"
     email["To"] = receiver
     email["Subject"] = subject
 
     text_part = MIMEText(message, "plain")
     email.attach(text_part)
 
-    context = ssl.create_default_context()
+    # Add attachments if any
+    if attachments:
+        for attachment in attachments:
+            email.attach(attachment)
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender, password)
-        server.sendmail(receiver, sender, email.as_string())
+    context = ssl.create_default_context()
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(email_address, email_password)
+            server.sendmail(email_address, receiver, email.as_string())
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+
+    return True
