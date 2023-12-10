5bd73df8f7635d47d1421599b68d1a7eab8d7632
Amygdala73 <introduction2algorithms@gmail.com>
2023-12-10 13:25:58 +1100

    receiver

--- frontend/src/components/aaa.js
+++ frontend/src/components/aaa.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Receiver from './Receiver';
+import { FaRobot } from 'react-icons/fa'; // Ensure FaRobot is imported if you're using it
+
 function Aaa() {
   return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      This is Receiver Page
-      <div >
-        <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded-md">
-          <Link to="/strategy" >
-              Next
-          </Link>  
-        </button>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <p>This is the Receiver Page.</p>
+      <div className="flex flex-row justify-center w-full px-4 mt-6">
+        <div className="flex-1">
+          <Receiver />
+        </div>
       </div>
-</div>
+      
+      <div style={{ marginTop: 'auto', textAlign: 'center' }}>
+        <Link to="/strategy" className="py-2 px-4 font-bold text-white bg-red-500 rounded-full hover:bg-red-700">
+          <FaRobot className="inline-block mr-2" />
+          Next
+        </Link>
+      </div>
+    </div>
   );
-};
+}
 
 export default Aaa;
