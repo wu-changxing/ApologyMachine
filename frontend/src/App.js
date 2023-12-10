f44d200ab0690510a64c6920e9fcfb481628349c
Amygdala73 <introduction2algorithms@gmail.com>
2023-12-10 11:48:14 +1100

    receiver

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./components/Home"; // Import the new Home component
 import Landing from "./components/Landing"; // Import the new Landing component
 import Login from "./components/Login"; // Import the new Login component
 import Strategy from "./components/Strategy"; // Import the new Strategy component
