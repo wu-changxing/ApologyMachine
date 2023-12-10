debcf841bc5c79b7730959d4ea35ef0507c43bcf
Your Name <email@example.com>
2023-12-10 16:23:35 +1100

    update

--- frontend/src/components/Strategy.js
+++ frontend/src/components/Strategy.js
@@ -21,9 +21,15 @@ const Strategy = () => {
     const [hoveredStrategy, setHoveredStrategy] = useState('');
     const [clickedStrategyDescription, setClickedStrategyDescription] = useState('');
     const strategyDescriptions = {
-        // ... strategy descriptions
-    };
-
+        'Polite Regret Waltz': "So, I heard I messed up. If that's a big deal to you, my bad. If not, cool. Either way, here's an apology, sort of.",
+        'Wonderland': "🙈✨ Heyyyyyy, so, I guess I need to, like, apologize or whatever? 😅 My bad for the thing that happened, or didn't happen... I'm, like, totally not keeping track. 🤷‍♂️ Can you feel the overwhelming insincerity radiating from this emoji extravaganza? 🌈😂 It's like a rainbow of fake apologies. 🌈🤮 Let's sweep this under the rug and move on to a world where sincerity is overrated. 🧹🚶‍♂️",
+        'Illusory Elegance': "I suppose I owe you an apology or something. Sorry for... you know, stuff. It's not entirely clear what, but let's just call it even and move on. Cool?",
+        'Genuine Facade': "I'm sorry if you're under the illusion that I care deeply about this. Let me assure you, it's an illusion, and I'm not sorry for being indifferent.",
+        'Cosmic Lullaby': "Zorblonk flibber-glabber, my fellow blorptoid! Apologorg for the glibberflorp in the zorblat continuum. Glip-glop, wizzle-wazzle, and may our frobnosticators align in the snarfleth dimension. Blork-blork!",
+        'Eco': "I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry, I’m sorry...",
+        'Sorry Flood Melody': "My bad for that sorry, and my bad for apologising about saying sorry. Actually, my bad for everything, whether it happened or not. Sorry for this text, and sorry for any texts in the future. Just really, sincerely, ridiculously sorry.",
+        'Deliberate Cadence': "In accordance with the socio-professional dynamics of the contextual paradigm, I hereby extend my apologies for the inadvertent alignment deviation.",
+    }
     const handleStrategySelect = (strategy) => {
         setSelectedStrategy(strategy);
         setClickedStrategyDescription(strategyDescriptions[strategy]);
