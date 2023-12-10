440624eb2faa6ddadfb09d44ec324b2569459cce
aaron <changxing.wu@gmx.com>
2023-12-10 16:01:28 +1100

    update the receiver

--- frontend/src/components/Receiver.js
+++ frontend/src/components/Receiver.js
@@ -15,64 +15,77 @@ const Receiver = () => {
   const handlePhoneNumberChange = (e) => setPhoneNumber(e.target.value);
   const handleCareerChange = (e) => setCareer(e.target.value);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    localStorage.setItem('receiverName', name);
-    localStorage.setItem('receiverEmail', email);
-    localStorage.setItem('receiverAddress', address);
-    localStorage.setItem('receiverPhoneNumber', phoneNumber);
-    localStorage.setItem('receiverCareer', career);
 
     if (name && email && address && phoneNumber && career) {
-      navigate("/strategy");
+      try {
+        const response = await fetch('http://localhost:8000/flood-victim', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name, email, address, phoneNumber, career
+          }),
+        });
+
+        if (response.ok) {
+          console.log('Data successfully sent to the server');
+          navigate("/strategy");
+        } else {
+          console.error('Failed to send data');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
     } else {
       alert("All fields are required!");
     }
   };
 
   return (
-      <div className="register-container" style={{ margin: '20px auto', maxWidth: '300px' }}>
-        <form onSubmit={handleSubmit} className="register-form" style={{ textAlign: 'left' }}>
-          <h2 style={{ textAlign: 'center', marginBottom: '20px' }}>we will use these info to help you have a better apology to your best friend!</h2>
+      <div className="flex justify-center items-center h-screen bg-gray-100">
+        <form onSubmit={handleSubmit} className="w-full max-w-lg bg-white p-8 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-semibold text-center text-gray-800 mb-8">Help us craft the perfect apology for your friend</h2>
 
           {/* Name Input */}
-          <div className="input-group" style={{ marginBottom: '10px' }}>
-            <label htmlFor="name" style={{ display: 'block' }}>Name</label>
-            <input type="text" id="name" name="name" value={name} onChange={handleNameChange} style={inputStyle} />
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">Name</label>
+            <input type="text" id="name" name="name" value={name} onChange={handleNameChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
           </div>
 
           {/* Email Input */}
-          <div className="input-group" style={{ marginBottom: '10px' }}>
-            <label htmlFor="email" style={{ display: 'block' }}>Email</label>
-            <input type="email" id="email" name="email" value={email} onChange={handleEmailChange} style={inputStyle} />
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email</label>
+            <input type="email" id="email" name="email" value={email} onChange={handleEmailChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
           </div>
 
           {/* Address Input */}
-          <div className="input-group" style={{ marginBottom: '10px' }}>
-            <label htmlFor="address" style={{ display: 'block' }}>Address</label>
-            <input type="text" id="address" name="address" value={address} onChange={handleAddressChange} style={inputStyle} />
+          <div className="mb-4">
+            <label htmlFor="address" className="block text-gray-700 text-sm font-bold mb-2">Address</label>
+            <input type="text" id="address" name="address" value={address} onChange={handleAddressChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
           </div>
 
           {/* Phone Number Input */}
-          <div className="input-group" style={{ marginBottom: '10px' }}>
-            <label htmlFor="phoneNumber" style={{ display: 'block' }}>Phone Number</label>
-            <input type="tel" id="phoneNumber" name="phoneNumber" value={phoneNumber} onChange={handlePhoneNumberChange} style={inputStyle} />
+          <div className="mb-4">
+            <label htmlFor="phoneNumber" className="block text-gray-700 text-sm font-bold mb-2">Phone Number</label>
+            <input type="tel" id="phoneNumber" name="phoneNumber" value={phoneNumber} onChange={handlePhoneNumberChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
           </div>
 
           {/* Career Input */}
-          <div className="input-group" style={{ marginBottom: '10px' }}>
-            <label htmlFor="career" style={{ display: 'block' }}>Career</label>
-            <input type="text" id="career" name="career" value={career} onChange={handleCareerChange} style={inputStyle} />
+          <div className="mb-4">
+            <label htmlFor="career" className="block text-gray-700 text-sm font-bold mb-2">Career</label>
+            <input type="text" id="career" name="career" value={career} onChange={handleCareerChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
           </div>
 
-          <button type="submit" className="py-2 px-4 font-bold text-white bg-red-500 rounded-full hover:bg-red-700 inline-flex items-center justify-center">
+          <button type="submit" className="w-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
             Apology now!
           </button>
         </form>
       </div>
   );
 };
-
 const inputStyle = { width: '100%', padding: '8px', border: '1px solid #ccc', borderRadius: '4px' };
 
 export default Receiver;
