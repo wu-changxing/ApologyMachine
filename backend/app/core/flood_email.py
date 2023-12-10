5f08709c91f31e41252f015cfd94f64e985fcc35
Jason Loi <z5420621@unsw.edu.au>
2023-12-10 15:22:26 +1100

    flood email with raspberry pi led flicker

--- backend/app/core/flood_email.py
+++ backend/app/core/flood_email.py
@@ -4,6 +4,64 @@ import ssl
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from dotenv import load_dotenv
+import RPi.GPIO as GPIO
+import time
+
+# Raspberry PI LED code
+GPIO.setmode(GPIO.BCM)
+
+
+led_pins = [18, 23, 24, 25]
+
+
+display_pins = [2, 3, 4, 17, 27, 22, 10]
+
+
+for pin in led_pins:
+    GPIO.setup(pin, GPIO.OUT)
+
+
+for pin in display_pins:
+    GPIO.setup(pin, GPIO.OUT)
+
+
+number_patterns = {
+    0: [1, 1, 1, 1, 1, 1, 0],
+    1: [0, 1, 1, 0, 0, 0, 0],
+    2: [1, 1, 0, 1, 1, 0, 1],
+    3: [1, 1, 1, 1, 0, 0, 1],
+    4: [0, 1, 1, 0, 0, 1, 1],
+    5: [1, 0, 1, 1, 0, 1, 1],
+    6: [1, 0, 1, 1, 1, 1, 1],
+    7: [1, 1, 1, 0, 0, 0, 0],
+    8: [1, 1, 1, 1, 1, 1, 1],
+    9: [1, 1, 1, 1, 0, 1, 1]
+}
+
+def flicker_leds(signal):
+
+    counter = 0
+
+
+    while True:
+        if signal == True:
+
+            GPIO.output(led_pins, GPIO.HIGH)
+
+
+            display_pattern = number_patterns[counter]
+            GPIO.output(display_pins, display_pattern)
+
+            counter = (counter + 1) % 10
+
+            time.sleep(0.5)
+            GPIO.output(led_pins, GPIO.LOW)
+            GPIO.output(display_pins, [0] * 7)
+            time.sleep(0.5)
+        else:
+            GPIO.output(led_pins, GPIO.LOW)
+            GPIO.output(display_pins, [0] * 7)
+
 
 load_dotenv(dotenv_path=".env.local")
 
@@ -45,7 +103,7 @@ def send_email(receiver, subject, message, attachments=None):
             print(f"An error occurred: {e}")
             return False
 
-    return True
+        # flicker LED each send
+        flicker_leds(True)
 
-if __name__ == "__main__":
-    send_email("leoligrcp@gmail.com", "apology", "sadness")
+    return True
