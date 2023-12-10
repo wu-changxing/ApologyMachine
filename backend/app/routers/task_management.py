b8ea8d8e5bb61a33d74ce1689724650cd57dcf05
aaron <changxing.wu@gmx.com>
2023-12-10 15:40:17 +1100

    first email

--- backend/app/routers/task_management.py
+++ backend/app/routers/task_management.py
@@ -33,9 +33,28 @@ async def submit_user_data(user_input: UserInput):
     # print(f"Received user data: {user_input.username}, {user_input.email_address}, {user_input.receiver}, {user_input.receiver_email_address}, {user_input.message}")
 
     # email formatting
-    apology_subject = f"Apology to {user_input.receiver}"
+    apology_subject = f" welcome to use Apology Machine  to {user_input.from_user}"
     email_message = user_input.message + f"\nMy sincerest apologies, {user_input.username}"
     
+    # job = queue.enqueue(send_email, user_input.receiver_email_address, apology_subject, user_input.message)
+
+    email = send_email(user_input.from_email, apology_subject, email_message)
+
+    # return {"message" : f"Received user data: {user_input.receiver}, {user_input.receiver_email_address}, {user_input.message}"}
+
+    return {"message": email}
+
+    # return {"message": "User data received successfully."}
+
+
+@router.post("/submit-user-data")
+async def submit_user_data(user_input: UserInput):
+    # print(f"Received user data: {user_input.username}, {user_input.email_address}, {user_input.receiver}, {user_input.receiver_email_address}, {user_input.message}")
+
+    # email formatting
+    apology_subject = f"Apology to {user_input.receiver}"
+    email_message = user_input.message + f"\nMy sincerest apologies, {user_input.username}"
+
     # job = queue.enqueue(send_email, user_input.receiver_email_address, apology_subject, user_input.message)
 
     email = send_email(user_input.receiver_email_address, apology_subject, email_message)
@@ -43,9 +62,6 @@ async def submit_user_data(user_input: UserInput):
     # return {"message" : f"Received user data: {user_input.receiver}, {user_input.receiver_email_address}, {user_input.message}"}
 
     return {"message": email}
-
-    # return {"message": "User data received successfully."}
-    
 # we URL
 # scan wifi
 @router.get("/scan", response_model=dict)
@@ -59,7 +75,6 @@ async def begin_scan(page: int = Query(1, alias="page"), pageSize: int = Query(1
 async def flood_essid(essids: str):
     # Split the long string into a list of sentences
     sentences = [essids]  # In this case, it's just one long sentence
-
     # Enqueue the flood_wifi_AP task with the sentences
     job = queue.enqueue(flood_wifi_AP, sentences)
 
