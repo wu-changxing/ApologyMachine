3ce7d2f76a1131ceddb3266452885bfe84aff7e7
Your Name <email@example.com>
2023-12-10 13:22:05 +1100

    link

--- frontend/src/components/bbb.js
+++ frontend/src/components/bbb.js
@@ -16,3 +16,21 @@ function Bbb() {
   );
 };
 export default Bbb;
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// function Aaa() {
+//   return (
+//     <div style={{ padding: '20px', textAlign: 'center' }}>
+//       This is Receiver Page
+//       <div >
+//         <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded-md">
+//           <Link to="/strategy" >
+//               Next
+//           </Link>  
+//         </button>
+//       </div>
+// </div>
+//   );
+// };
+
+// export default Aaa;
